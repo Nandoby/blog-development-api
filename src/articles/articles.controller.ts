f6e3a77533d5ad144452f2ad7c0bf96514d5b281
Biaccalli Ferdinando <nandobiaccalli@gmail.com>
2023-06-07 12:20:39 +0200

    Mise en place AuthGuard, application sur les articles

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/createArticle.dto';
 import { UpdateArticleDto } from './dto/updateArticle.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('articles')
 export class ArticlesController {
@@ -12,6 +13,7 @@ export class ArticlesController {
     return await this.articlesService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
@@ -22,11 +24,13 @@ export class ArticlesController {
     return this.articlesService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async update(@Param('id') id: string, @Body() updatedArticle: UpdateArticleDto) {
     return await this.articlesService.update(id, updatedArticle);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     await this.articlesService.remove(id);
--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -4,10 +4,11 @@ import { ArticlesService } from './articles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from './article.entity';
 import { Category } from 'src/categories/category.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Article, Category])],
   controllers: [ArticlesController],
-  providers: [ArticlesService],
+  providers: [ArticlesService, JwtService],
 })
 export class ArticlesModule {}
