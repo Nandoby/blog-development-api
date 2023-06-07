9e798515bff723dea356670cd9a339a8872b2b33
Biaccalli Ferdinando <nandobiaccalli@gmail.com>
2023-06-07 11:30:25 +0200

    Mise en place JWT + configuration avec variable d'environnement

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { SignInDto } from './dto/signIn.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(signInDto: SignInDto): Promise<any> {
     const user = await this.usersService.findByUsername(signInDto.username);
@@ -14,12 +18,12 @@ export class AuthService {
     const loggedIn = await bcrypt.compare(signInDto.password, user.password);
 
     if (loggedIn) {
-      const { password, ...result } = user;
-      // TODO: Generate a JWT and return it here
-      // instead of the user object
-      return user;
+      const payload = { sub: user.id, username: user.username }
+      return {
+        access_token: await this.jwtService.signAsync(payload)
+      }
     } else {
-        throw new UnauthorizedException();
+      throw new UnauthorizedException();
     }
   }
 }
